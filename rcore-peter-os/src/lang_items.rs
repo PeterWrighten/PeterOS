25cfcd29ef74991fbbed9da17f33f542e3b7deef
PeterWright <peterwrighten@gmail.com>
2022-01-17 01:06:46 +0900

    update::doc

--- rcore-peter-os/src/lang_items.rs
+++ rcore-peter-os/src/lang_items.rs
@@ -1,7 +1,10 @@
+// 'lang_items' mod, without std support.
+
+// use PanicInfo to rebuild "panic handler"
 use core::panic::PanicInfo;
 
 
-
+// &PanicInfo is message of Panic from compiler.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
--- rcore-peter-os/src/main.rs
+++ rcore-peter-os/src/main.rs
@@ -1,6 +1,10 @@
+// Move away standard library
 #![no_std]
+
+// Move away 'main()' function
 #![no_main]
 
+// Invoke lang_items mod to implement "panic handler"
 mod lang_items;
 
 
