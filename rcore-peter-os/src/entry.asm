14f7573eb4d5cd6b96571612cb3f2a7b727405df
PeterWright <peterwrighten@gmail.com>
2022-01-17 21:56:05 +0900

    update::entry.asn

--- rcore-peter-os/src/entry.asm
+++ rcore-peter-os/src/entry.asm
@@ -1,4 +1,12 @@
     .section .text.entry
     .globl _start
 _start:
-    li x1, 100
+    la sp, boot_stack_top
+    call rust_main
+
+    .section .bss.stack
+    .global boot_stack
+boot_stack:
+    .space 4096 * 16
+    .global boot_stack_top
+boot_stack_top:
--- rcore-peter-os/src/main.rs
+++ rcore-peter-os/src/main.rs
@@ -10,6 +10,9 @@ mod lang_items;
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
-
+#[no_mangle]
+pub fn rust_main() -> ! {
+    loop{}
+}
 
 
