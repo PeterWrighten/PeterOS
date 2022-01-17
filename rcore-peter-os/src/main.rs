bda1fbbb486422c126d65e70f6de5a3d1bb03394
PeterWright <peterwrighten@gmail.com>
2022-01-17 23:19:47 +0900

    update::Layout stack frame

--- rcore-peter-os/src/main.rs
+++ rcore-peter-os/src/main.rs
@@ -12,7 +12,20 @@ global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    clear_bss();
     loop{}
+}
+
+fn clear_bss() {
+    extern "C" {// extern means the call of extern Interface of other Programming language.
+        // functional call Interface in RISC-V for C language.
+        // inorder to read sbss and ebss's address.
+        fn sbss(); 
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a|{
+        unsafe {(a as *mut u8).write_volatile(0)}
+    });
 }
 
 
