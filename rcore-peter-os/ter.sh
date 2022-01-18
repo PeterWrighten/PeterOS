f8417afafe0ed4663966e759d6bb33291806ea3d
PeterWright <peterwrighten@gmail.com>
2022-01-18 15:22:15 +0900

    init

--- rcore-peter-os/ter.sh
+++
@@ -1,6 +0,0 @@
-qemu-system-riscv64 \
-	-machine virt \
-	-nographic \
-	-bios bootloader/rustsbi-qemu.bin \
-	-device loader,file=target/riscv64gc-unknown-none-elf/release/rcore-peter-os.bin,addr=0x80200000 
-	
