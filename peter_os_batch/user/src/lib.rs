039a5b93b7c0d1d0ed4ccc88e1a412c1031d0910
PeterWright <peterwrighten@gmail.com>
2022-01-19 21:31:53 +0900

    update::peter_os_batch::lib

--- peter_os_batch/user/src/lib.rs
+++ peter_os_batch/user/src/lib.rs
@@ -3,6 +3,12 @@
 #![feature(panic_info_message)]
 
 
+
+#[macro_use]
+pub mod console;
+mod syscall;
+mod lang_items;
+
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
@@ -10,3 +16,14 @@ pub extern "C" fn _start() -> ! {
     exit(main());
     panic!("unreachable after sys_exit!");
 }
+
+fn clear_bss() {
+    extern "C" {
+        fn start_bss();
+        fn end_bss();
+    }
+    (start_bss as usize..end_bss as usize).for_each(|addr| {
+        unsafe { (addr as *mut u8).write_volatile(0); }
+    });
+}
+
