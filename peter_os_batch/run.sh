a54a148538dffaf903ae94dea53229a1b7ee0b28
PeterWright <peterwrighten@gmail.com>
2022-01-21 15:11:20 +0900

    del::old_one

--- peter_os_batch/run.sh
+++
@@ -1,5 +0,0 @@
-qemu-system-riscv64 \
-    -machine virt \
-    -nographic \
-    -bios bootloader/rustsbi-qemu.bin \
-    -device loader,file=target/riscv64gc-unknown-none-elf/release/peter_os_batch.bin,addr=0x80400000
--- peter_os_batch/user.sh
+++
@@ -1,5 +0,0 @@
-qemu-system-riscv64 \
-    -machine virt \
-    -nographic \
-    -bios bootloader/rustsbi-qemu.bin \
-    -device loader,file=user/target/riscv64gc-unknown-none-elf/release/hello_world,addr=0x80400000
