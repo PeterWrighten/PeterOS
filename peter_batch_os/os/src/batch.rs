a73ff3eb3a90368475fd79a4fe7c1402b3d2c816
PeterWright <peterwrighten@gmail.com>
2022-01-23 01:27:01 +0900

    update::batch.rs

--- peter_batch_os/os/src/batch.rs
+++ peter_batch_os/os/src/batch.rs
@@ -1,11 +1,25 @@
 
-use std::cell::RefCell;
-// static A: RefCell<i32> = RefCell::new(3);
+use crate::sync::UPSafeCell;
+use core::arch::asm;
+use lazy_static::*;
 
-
+const USER_STACK_SIZE: usize = 4096 * 2;
+const KERNEL_STACK_SIZE: usize = 4096 * 2;
+const MAX_APP_NUM: usize = 16;
+const APP_BASE_ADDRESS: usize = 0x80400000;
+const APP_SIZE_LIMIT: usize = 0x20000;
 
 struct AppManager {
     num_app: usize,
     current_app: usize,
     app_start: [usize; MAX_APP_NUM + 1],
+}
+
+lazy_static! {
+    static ref APP_MANAGER: UPSafeCell<AppManager> =unsafe { UPSafeCell::new({
+        extern "C" { fn _num_app(); }
+        let num_app_ptr = _num_app as usize as *const usize;
+        let num_app = num_app_ptr.read_volatile();
+        
+    })};
 }
