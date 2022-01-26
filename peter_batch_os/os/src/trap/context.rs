8f4c72c53951a40ef1befb235bf551d65e3d05aa
PeterWright <peterwrighten@gmail.com>
2022-01-26 17:34:40 +0900

    update::trap

--- peter_batch_os/os/src/trap/context.rs
+++ peter_batch_os/os/src/trap/context.rs
@@ -2,7 +2,8 @@ use riscv::register::sstatus::{self, Sstatus, SPP};
 
 #[repr(C)]
 pub struct TrapContext {
-    pub x: [usize; 32],
+    pub x: [usize; 32],//General Register
     pub sstatus: Sstatus,
     pub sepc: usize,
 }
+
