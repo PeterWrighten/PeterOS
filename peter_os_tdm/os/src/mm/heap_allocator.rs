c2bef9700fd84ea4aa4a51ed87f103231b289db0
PeterWrighten <peterwrighten@gmail.com>
2022-02-19 03:49:23 +0900

    update::alloc::test

--- peter_os_tdm/os/src/mm/heap_allocator.rs
+++ peter_os_tdm/os/src/mm/heap_allocator.rs
@@ -28,8 +28,19 @@ pub fn heap_test() {
         fn ebss();
     }
     let bss_range = sbss as usize..ebss as usize;
-    let &a = Box::new(5);
-    assert_eq!(a, 5);
-    drop(&a);
+    let a = Box::new(5);
+    assert_eq!(*a, 5);
+    assert!(bss_range.contains(&(a.as_ref() as *const _ as usize)));
+    drop(a);
+    let mut v: Vec<usize> = Vec::new();
+    for i in 0..500 {
+        v.push(i);
+    }
+    for (i, val) in v.iter().take(500).enumerate() {
+        assert_eq!(*val, i);
+    }
+    assert!(bss_range.contains(&(v.as_ptr() as usize)));
+    drop(v);
+    println!("heap_test_passed!");
 
 }
