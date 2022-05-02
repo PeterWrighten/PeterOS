2d19f8c7a7a9e539cb175502cda4b22d1553ca9a
PeterWrighten <peterwrighten@gmail.com>
2022-05-03 07:10:08 +0900

    Update: Trap, TaskControlBlock

--- peter_os_tdm/os/src/trap/context.rs
+++ peter_os_tdm/os/src/trap/context.rs
@@ -4,9 +4,36 @@
 pub struct TrapContext {
     pub x: [usize; 32],
     pub sstatus: Sstatus,
-    pub sepc: usize,
+    pub sepc: usize, // save latest address before trap
     pub kernel_satp: usize, // token of kernel address space, start of kernel pagetable
     pub kernel_sp: usize, // initial address of kernel space
     pub trap_handler: usize, // start of trap handler
+}
+
+impl TrapContext {
+    pub fn set_sp(&mut self, sp: usize) {
+        self.x[2] = sp;
+    }
+
+    pub fn app_init_context(
+        entry: usize,
+        sp: usize,
+        kernel_satp: usize,
+        kernel_sp: usize,
+        trap_handler: usize,
+    ) -> Self {
+        let mut sstatus = sstatus::read();
+        sstatus.set_spp(SPP::User);
+        let mut cx = Self {
+            x: [0; 32],
+            sstatus,
+            sepc: entry,
+            kernel_satp,
+            kernel_sp,
+            trap_handler,
+        };
+        cx.set_sp(sp);
+        cx
+    }
 }
 
