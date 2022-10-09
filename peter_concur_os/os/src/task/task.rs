b255af4b3ad5166299ba500e85e79c5b3b51676e
PeterWrighten <peterwrighten@gmail.com>
2022-10-09 18:55:04 +0900

    update: thread block

--- peter_concur_os/os/src/task/task.rs
+++ peter_concur_os/os/src/task/task.rs
@@ -7,5 +7,15 @@ pub struct TaskControlBlock {
 }
 
 pub struct TaskControlBlockInner {
-    
+    pub res: Option<TaskUserRes>,
+    pub trap_cx_ppn: PhysPageNum,
+    pub task_cx: TaskContext,
+    pub task_status: TaskStatus,
+    pub exit_code: Option<i32>,
+}
+
+pub struct TaskUserRes {
+    pub tid: usize,
+    pub ustack_base: usize,
+    pub process: Weak<ProcessControlBlock>,
 }
